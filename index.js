b5de3cd0adf940b2c7d78d7d13e4b97835bc1025
dhulsingh12 <dhulsingh1@gmail.com>
2023-02-16 12:52:21 +0000

    congratsCArdApp

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  // Write your code here.
+  <div className="bgcontainer">
+    <h1>congratulation</h1>
+    <div className="backcolor">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      ;<h1 className="head">Dhulsingh</h1>
+      <p className="pere"> cvr college of Engineering at hyderadad</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+      ;
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
